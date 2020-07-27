4cbc9937c430a024b042bb1f75f896dde0ae8c16
grantorchard <go@hashicorp.com>
2020-07-27 21:06:00 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,1 @@
-# GCP General Configuration
-provider "google" {
-  version = "~> 3.5"
-  project = var.gcp_project
-  region  = var.gcp_region
-}
 
-# Azure General Configuration
-provider "azurerm" {
-  version = "~>1.42.0"
-}
